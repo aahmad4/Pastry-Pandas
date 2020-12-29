cc5cf5498ca672f51a8ee6601f812bb629338020
Ali Ahmad <alislearning123@gmail.com>
2020-12-29 18:41:42 -0500

    Created initial shopping page with items fetched from Commerce JS Api and created initial cart page with items added to Commerce JS Cart

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+
+import logo from "../../assets/panda.png";
+import useStyles from "./styles";
+
+export default function Navbar({ totalItems }) {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              src={logo}
+              alt="Pastry Pandas"
+              height="25px"
+              className={classes.image}
+            />
+            Pastry Pandas
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={totalItems} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+}
